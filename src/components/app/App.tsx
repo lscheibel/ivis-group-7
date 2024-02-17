15b983fabfd4e44450b3830cd283c0efbff2d52e
Lennard Scheibel <lscheibel@Lennards-MacBook-Air.local>
2024-02-17 11:33:17 +0100

    Add minimal router

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,13 +2,26 @@ import React from 'react';
 import styles from './App.module.scss';
 import DashboardView from '../dashboard/DashboardView/DashboardView';
 import { useSelectedCountry } from '../../state/selectedCountry';
+import { Link, Route } from 'wouter';
 
 const App = () => {
     const selectedCountry = useSelectedCountry();
 
     return (
         <div className={styles.wrapper}>
-            <DashboardView exampleProp={selectedCountry} />
+            Navigation:{' '}
+            <ul>
+                <li>
+                    <Link href={'/'}>Data</Link>
+                </li>
+                <li>
+                    <Link href={'/about'}>About</Link>
+                </li>
+            </ul>
+            <Route path={'/about'}>About us, Todo.</Route>
+            <Route path={'/'}>
+                <DashboardView exampleProp={selectedCountry} />
+            </Route>
         </div>
     );
 };
