1260d83b3e728c6de4a905f2436f75df207513e1
Lennard Scheibel <contact@lennardscheibel.de>
2024-03-02 14:20:40 +0100

    Move v2 data back to data and instead deprecate some properties

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,12 +1,6 @@
 import jsonData from './data.json';
 import { StringParser } from './StringParser';
 
-console.warn(`
-┌─────────────────────────────────────────────────────────────┐
-│ WARNING: Import of deprecated data detected. Please use v2. │
-└─────────────────────────────────────────────────────────────┘
-`);
-
 export interface RawDataEntry {
     country: string;
     countryCode: string;
@@ -51,6 +45,54 @@ export interface RawDataEntry {
     availableFoodMaize: string;
     availableFoodRice: string;
     availableFoodWheat: string;
+
+    foodGramsPerDayFruits: string;
+    foodGramsPerDayNonStarchyVegetables: string;
+    foodGramsPerDayPotatoes: string;
+    foodGramsPerDayOtherStarchyVegetables: string;
+    foodGramsPerDayBeansAndLegumes: string;
+    foodGramsPerDayNutsAndSeeds: string;
+    foodGramsPerDayRefinedGrains: string;
+    foodGramsPerDayWholeGrains: string;
+    foodGramsPerDayProcessedMeats: string;
+    foodGramsPerDayUnprocessedRedMeats: string;
+    foodGramsPerDaySeafoods: string;
+    foodGramsPerDayEggs: string;
+    foodGramsPerDayCheese: string;
+    foodGramsPerDayYoghurt: string;
+    foodGramsPerDaySugarSweetenedBeverages: string;
+    foodGramsPerDayFruitJuices: string;
+    foodGramsPerDayTea: string;
+    foodGramsPerDaySaturatedFat: string;
+
+    percentageOfSchoolTimeSpentUsingDigitalDevices: string;
+
+    pisaMathScore2003: string;
+    pisaMathScore2006: string;
+    pisaMathScore2009: string;
+    pisaMathScore2012: string;
+    pisaMathScore2015: string;
+    pisaMathScore2018: string;
+    pisaMathScore2022: string;
+    pisaReadingScore2003: string;
+    pisaReadingScore2006: string;
+    pisaReadingScore2009: string;
+    pisaReadingScore2012: string;
+    pisaReadingScore2015: string;
+    pisaReadingScore2018: string;
+    pisaReadingScore2022: string;
+    pisaScienceScore2006: string;
+    pisaScienceScore2009: string;
+    pisaScienceScore2012: string;
+    pisaScienceScore2015: string;
+    pisaScienceScore2018: string;
+    pisaScienceScore2022: string;
+    pisaGeneralAverage2006: string;
+    pisaGeneralAverage2009: string;
+    pisaGeneralAverage2012: string;
+    pisaGeneralAverage2015: string;
+    pisaGeneralAverage2018: string;
+    pisaGeneralAverage2022: string;
 }
 
 export class CountryDatum {
@@ -59,9 +101,13 @@ export class CountryDatum {
     countryName: string;
     countryCode: string;
 
+    /** @deprecated Use year specific scores instead. **/
     private pisaMathScore: number;
+    /** @deprecated Use year specific scores instead. **/
     private pisaScienceScore: number;
+    /** @deprecated Use year specific scores instead. **/
     private pisaReadingScore: number;
+    /** @deprecated Use year specific scores instead. **/
     private pisaGeneralAverageScore: number;
 
     private skippedMealsNever: number | null;
@@ -74,48 +120,101 @@ export class CountryDatum {
     senseOfBelonging: number | null;
     parentsFrequentlyAskingAboutDay: number | null;
 
-    availableFoodMiscellaneous: number | null;
-    availableFoodAlcoholicBeverages: number | null;
-    availableFoodAnimalFats: number | null;
-    availableFoodVegetableOils: number | null;
-    availableFoodOilCrops: number | null;
-    availableFoodFishAndSeafood: number | null;
-    availableFoodSugarCrops: number | null;
-    availableFoodSugarAndSweeteners: number | null;
-    availableFoodStarchyRoots: number | null;
-    availableFoodOtherMeat: number | null;
-    availableFoodMeatSheepAndGoat: number | null;
-    availableFoodMeatPig: number | null;
-    availableFoodMeatPoultry: number | null;
-    availableFoodMeatBeef: number | null;
-    availableFoodEggs: number | null;
-    availableFoodMilk: number | null;
-    availableFoodNuts: number | null;
-    availableFoodFruit: number | null;
-    availableFoodVegetables: number | null;
-    availableFoodPulses: number | null;
-    availableFoodCerealsOther: number | null;
-    availableFoodBarley: number | null;
-    availableFoodMaize: number | null;
-    availableFoodRice: number | null;
-    availableFoodWheat: number | null;
+    private availableFoodMiscellaneous: number | null;
+    private availableFoodAlcoholicBeverages: number | null;
+    private availableFoodAnimalFats: number | null;
+    private availableFoodVegetableOils: number | null;
+    private availableFoodOilCrops: number | null;
+    private availableFoodFishAndSeafood: number | null;
+    private availableFoodSugarCrops: number | null;
+    private availableFoodSugarAndSweeteners: number | null;
+    private availableFoodStarchyRoots: number | null;
+    private availableFoodOtherMeat: number | null;
+    private availableFoodMeatSheepAndGoat: number | null;
+    private availableFoodMeatPig: number | null;
+    private availableFoodMeatPoultry: number | null;
+    private availableFoodMeatBeef: number | null;
+    private availableFoodEggs: number | null;
+    private availableFoodMilk: number | null;
+    private availableFoodNuts: number | null;
+    private availableFoodFruit: number | null;
+    private availableFoodVegetables: number | null;
+    private availableFoodPulses: number | null;
+    private availableFoodCerealsOther: number | null;
+    private availableFoodBarley: number | null;
+    private availableFoodMaize: number | null;
+    private availableFoodRice: number | null;
+    private availableFoodWheat: number | null;
+
+    private foodGramsPerDayFruits: number | null;
+    private foodGramsPerDayNonStarchyVegetables: number | null;
+    private foodGramsPerDayPotatoes: number | null;
+    private foodGramsPerDayOtherStarchyVegetables: number | null;
+    private foodGramsPerDayBeansAndLegumes: number | null;
+    private foodGramsPerDayNutsAndSeeds: number | null;
+    private foodGramsPerDayRefinedGrains: number | null;
+    private foodGramsPerDayWholeGrains: number | null;
+    private foodGramsPerDayProcessedMeats: number | null;
+    private foodGramsPerDayUnprocessedRedMeats: number | null;
+    private foodGramsPerDaySeafoods: number | null;
+    private foodGramsPerDayEggs: number | null;
+    private foodGramsPerDayCheese: number | null;
+    private foodGramsPerDayYoghurt: number | null;
+    private foodGramsPerDaySugarSweetenedBeverages: number | null;
+    private foodGramsPerDayFruitJuices: number | null;
+    private foodGramsPerDayTea: number | null;
+    private foodGramsPerDaySaturatedFat: number | null;
+
+    percentageOfSchoolTimeSpentUsingDigitalDevices: number | null;
+
+    private pisaMathScore2003: number | null;
+    private pisaMathScore2006: number | null;
+    private pisaMathScore2009: number | null;
+    private pisaMathScore2012: number | null;
+    private pisaMathScore2015: number | null;
+    private pisaMathScore2018: number | null;
+    private pisaMathScore2022: number;
+    private pisaReadingScore2003: number | null;
+    private pisaReadingScore2006: number | null;
+    private pisaReadingScore2009: number | null;
+    private pisaReadingScore2012: number | null;
+    private pisaReadingScore2015: number | null;
+    private pisaReadingScore2018: number | null;
+    private pisaReadingScore2022: number;
+    private pisaScienceScore2006: number | null;
+    private pisaScienceScore2009: number | null;
+    private pisaScienceScore2012: number | null;
+    private pisaScienceScore2015: number | null;
+    private pisaScienceScore2018: number | null;
+    private pisaScienceScore2022: number;
+    private pisaGeneralAverage2006: number | null;
+    private pisaGeneralAverage2009: number | null;
+    private pisaGeneralAverage2012: number | null;
+    private pisaGeneralAverage2015: number | null;
+    private pisaGeneralAverage2018: number | null;
+    private pisaGeneralAverage2022: number;
 
     constructor(id: number, raw: RawDataEntry) {
         this.id = id;
+
         this.countryName = raw.country;
         this.countryCode = raw.countryCode;
+
         this.pisaMathScore = StringParser.parseAsNumber(raw.pisaMathScore)!;
         this.pisaScienceScore = StringParser.parseAsNumber(raw.pisaScienceScore)!;
         this.pisaReadingScore = StringParser.parseAsNumber(raw.pisaReadingScore)!;
         this.pisaGeneralAverageScore = StringParser.parseAsNumber(raw.pisaGeneralAverageScore)!;
+
         this.skippedMealsNever = StringParser.parseAsNumber(raw.skippedMealsNever);
         this.skippedMealsOncePerWeek = StringParser.parseAsNumber(raw.skippedMealsOncePerWeek);
         this.skippedMealsTwoToThreePerWeek = StringParser.parseAsNumber(raw.skippedMealsTwoToThreePerWeek);
         this.skippedMealsFourToFivePerWeek = StringParser.parseAsNumber(raw.skippedMealsFourToFivePerWeek);
         this.skippedMealsAlways = StringParser.parseAsNumber(raw.skippedMealsAlways);
+
         this.familySupport = StringParser.parseAsNumber(raw.familySupport);
         this.senseOfBelonging = StringParser.parseAsNumber(raw.senseOfBelonging);
         this.parentsFrequentlyAskingAboutDay = StringParser.parseAsNumber(raw.parentsFrequentlyAskingAboutDay);
+
         this.availableFoodMiscellaneous = StringParser.parseAsNumber(raw.availableFoodMiscellaneous);
         this.availableFoodAlcoholicBeverages = StringParser.parseAsNumber(raw.availableFoodAlcoholicBeverages);
         this.availableFoodAnimalFats = StringParser.parseAsNumber(raw.availableFoodAnimalFats);
@@ -141,14 +240,110 @@ export class CountryDatum {
         this.availableFoodMaize = StringParser.parseAsNumber(raw.availableFoodMaize);
         this.availableFoodRice = StringParser.parseAsNumber(raw.availableFoodRice);
         this.availableFoodWheat = StringParser.parseAsNumber(raw.availableFoodWheat);
+
+        this.foodGramsPerDayFruits = StringParser.parseAsNumber(raw.foodGramsPerDayFruits);
+        this.foodGramsPerDayNonStarchyVegetables = StringParser.parseAsNumber(raw.foodGramsPerDayNonStarchyVegetables);
+        this.foodGramsPerDayPotatoes = StringParser.parseAsNumber(raw.foodGramsPerDayPotatoes);
+        this.foodGramsPerDayOtherStarchyVegetables = StringParser.parseAsNumber(
+            raw.foodGramsPerDayOtherStarchyVegetables
+        );
+        this.foodGramsPerDayBeansAndLegumes = StringParser.parseAsNumber(raw.foodGramsPerDayBeansAndLegumes);
+        this.foodGramsPerDayNutsAndSeeds = StringParser.parseAsNumber(raw.foodGramsPerDayNutsAndSeeds);
+        this.foodGramsPerDayRefinedGrains = StringParser.parseAsNumber(raw.foodGramsPerDayRefinedGrains);
+        this.foodGramsPerDayWholeGrains = StringParser.parseAsNumber(raw.foodGramsPerDayWholeGrains);
+        this.foodGramsPerDayProcessedMeats = StringParser.parseAsNumber(raw.foodGramsPerDayProcessedMeats);
+        this.foodGramsPerDayUnprocessedRedMeats = StringParser.parseAsNumber(raw.foodGramsPerDayUnprocessedRedMeats);
+        this.foodGramsPerDaySeafoods = StringParser.parseAsNumber(raw.foodGramsPerDaySeafoods);
+        this.foodGramsPerDayEggs = StringParser.parseAsNumber(raw.foodGramsPerDayEggs);
+        this.foodGramsPerDayCheese = StringParser.parseAsNumber(raw.foodGramsPerDayCheese);
+        this.foodGramsPerDayYoghurt = StringParser.parseAsNumber(raw.foodGramsPerDayYoghurt);
+        this.foodGramsPerDaySugarSweetenedBeverages = StringParser.parseAsNumber(
+            raw.foodGramsPerDaySugarSweetenedBeverages
+        );
+        this.foodGramsPerDayFruitJuices = StringParser.parseAsNumber(raw.foodGramsPerDayFruitJuices);
+        this.foodGramsPerDayTea = StringParser.parseAsNumber(raw.foodGramsPerDayTea);
+        this.foodGramsPerDaySaturatedFat = StringParser.parseAsNumber(raw.foodGramsPerDaySaturatedFat);
+
+        this.percentageOfSchoolTimeSpentUsingDigitalDevices = StringParser.parseAsNumber(
+            raw.percentageOfSchoolTimeSpentUsingDigitalDevices
+        );
+
+        this.pisaMathScore2003 = StringParser.parseAsNumber(raw.pisaMathScore2003);
+        this.pisaMathScore2006 = StringParser.parseAsNumber(raw.pisaMathScore2006);
+        this.pisaMathScore2009 = StringParser.parseAsNumber(raw.pisaMathScore2009);
+        this.pisaMathScore2012 = StringParser.parseAsNumber(raw.pisaMathScore2012);
+        this.pisaMathScore2015 = StringParser.parseAsNumber(raw.pisaMathScore2015);
+        this.pisaMathScore2018 = StringParser.parseAsNumber(raw.pisaMathScore2018);
+        this.pisaMathScore2022 = StringParser.parseAsNumber(raw.pisaMathScore2022)!;
+        this.pisaReadingScore2003 = StringParser.parseAsNumber(raw.pisaReadingScore2003);
+        this.pisaReadingScore2006 = StringParser.parseAsNumber(raw.pisaReadingScore2006);
+        this.pisaReadingScore2009 = StringParser.parseAsNumber(raw.pisaReadingScore2009);
+        this.pisaReadingScore2012 = StringParser.parseAsNumber(raw.pisaReadingScore2012);
+        this.pisaReadingScore2015 = StringParser.parseAsNumber(raw.pisaReadingScore2015);
+        this.pisaReadingScore2018 = StringParser.parseAsNumber(raw.pisaReadingScore2018);
+        this.pisaReadingScore2022 = StringParser.parseAsNumber(raw.pisaReadingScore2022)!;
+        this.pisaScienceScore2006 = StringParser.parseAsNumber(raw.pisaScienceScore2006);
+        this.pisaScienceScore2009 = StringParser.parseAsNumber(raw.pisaScienceScore2009);
+        this.pisaScienceScore2012 = StringParser.parseAsNumber(raw.pisaScienceScore2012);
+        this.pisaScienceScore2015 = StringParser.parseAsNumber(raw.pisaScienceScore2015);
+        this.pisaScienceScore2018 = StringParser.parseAsNumber(raw.pisaScienceScore2018);
+        this.pisaScienceScore2022 = StringParser.parseAsNumber(raw.pisaScienceScore2022)!;
+        this.pisaGeneralAverage2006 = StringParser.parseAsNumber(raw.pisaGeneralAverage2006);
+        this.pisaGeneralAverage2009 = StringParser.parseAsNumber(raw.pisaGeneralAverage2009);
+        this.pisaGeneralAverage2012 = StringParser.parseAsNumber(raw.pisaGeneralAverage2012);
+        this.pisaGeneralAverage2015 = StringParser.parseAsNumber(raw.pisaGeneralAverage2015);
+        this.pisaGeneralAverage2018 = StringParser.parseAsNumber(raw.pisaGeneralAverage2018);
+        this.pisaGeneralAverage2022 = StringParser.parseAsNumber(raw.pisaGeneralAverage2022)!;
     }
 
     get pisaScores() {
         return {
-            average: this.pisaGeneralAverageScore,
-            math: this.pisaMathScore,
-            reading: this.pisaReadingScore,
-            science: this.pisaScienceScore,
+            average: this.pisaGeneralAverage2022,
+            math: this.pisaMathScore2022,
+            reading: this.pisaReadingScore2022,
+            science: this.pisaScienceScore2022,
+            '2003': {
+                average: null,
+                math: this.pisaMathScore2003,
+                reading: this.pisaReadingScore2003,
+                science: null,
+            },
+            '2006': {
+                average: this.pisaGeneralAverage2006,
+                math: this.pisaMathScore2006,
+                reading: this.pisaReadingScore2006,
+                science: this.pisaScienceScore2006,
+            },
+            '2009': {
+                average: this.pisaGeneralAverage2009,
+                math: this.pisaMathScore2009,
+                reading: this.pisaReadingScore2009,
+                science: this.pisaScienceScore2009,
+            },
+            '2012': {
+                average: this.pisaGeneralAverage2012,
+                math: this.pisaMathScore2012,
+                reading: this.pisaReadingScore2012,
+                science: this.pisaScienceScore2012,
+            },
+            '2015': {
+                average: this.pisaGeneralAverage2015,
+                math: this.pisaMathScore2015,
+                reading: this.pisaReadingScore2015,
+                science: this.pisaScienceScore2015,
+            },
+            '2018': {
+                average: this.pisaGeneralAverage2018,
+                math: this.pisaMathScore2018,
+                reading: this.pisaReadingScore2018,
+                science: this.pisaScienceScore2018,
+            },
+            '2022': {
+                average: this.pisaGeneralAverage2022,
+                math: this.pisaMathScore2022,
+                reading: this.pisaReadingScore2022,
+                science: this.pisaScienceScore2022,
+            },
         };
     }
 
@@ -176,6 +371,7 @@ export class CountryDatum {
         );
     }
 
+    /** @deprecated Use foodInGrams instead. **/
     get availableFood() {
         return {
             miscellaneous: this.availableFoodMiscellaneous,
@@ -205,10 +401,30 @@ export class CountryDatum {
             wheat: this.availableFoodWheat,
         };
     }
+
+    get foodInGrams() {
+        // We always have either all the data or no data.
+        if (this.foodGramsPerDayFruits == null) return null;
+
+        return {
+            fruits: this.foodGramsPerDayFruits!,
+            vegetables:
+                this.foodGramsPerDayNonStarchyVegetables! +
+                this.foodGramsPerDayPotatoes! +
+                this.foodGramsPerDayOtherStarchyVegetables!,
+            grains: this.foodGramsPerDayRefinedGrains! + this.foodGramsPerDayWholeGrains!,
+            beans: this.foodGramsPerDayBeansAndLegumes!,
+            nuts: this.foodGramsPerDayNutsAndSeeds!,
+            meats: this.foodGramsPerDayProcessedMeats! + this.foodGramsPerDayUnprocessedRedMeats!,
+            seafoods: this.foodGramsPerDaySeafoods!,
+            dairyAndEggs: this.foodGramsPerDayEggs! + this.foodGramsPerDayCheese! + this.foodGramsPerDayYoghurt!,
+            sugaryDrinks: this.foodGramsPerDaySugarSweetenedBeverages! + this.foodGramsPerDayFruitJuices!,
+            tea: this.foodGramsPerDayTea!,
+        };
+    }
 }
 
 export const data = jsonData.map((rawEntry, index) => {
-    if (rawEntry.country === 'Cambodia') console.log(new CountryDatum(index, rawEntry));
     return new CountryDatum(index, rawEntry);
 });
 
