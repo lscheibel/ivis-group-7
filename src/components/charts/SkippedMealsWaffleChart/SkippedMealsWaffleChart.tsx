2414fd7c2a391a3ea32ad955c6959f9b291c39f4
Lennard Scheibel <lscheibel@Lennards-MacBook-Air.local>
2024-03-04 17:36:02 +0100

    Add waffle chart

---
+++ src/components/charts/SkippedMealsWaffleChart/SkippedMealsWaffleChart.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import styles from './SkippedMealsWaffleChart.module.scss';
+import ChartsWrapper from '../../ChartsWrapper/ChartsWrapper';
+import WaffleChart from '../WaffleChart/WaffleChart';
+import { CountryDatum } from '../../../data/data';
+import { mapRange } from '../../../tools/math';
+
+export interface SkippedMealsWaffleChartProps {
+    data: CountryDatum | null;
+}
+
+const getLabel = (key: string, value: number) => {
+    const formattedValue = value.toFixed(1);
+
+    switch (key) {
+        case 'never':
+            return `${formattedValue}% never skip a meal.`;
+        case 'oncePerWeek':
+            return `${formattedValue}% skip a meal once per week.`;
+        case 'twoToThreePerWeek':
+            return `${formattedValue}% skip a meal two to three times per week.`;
+        case 'fourToFivePerWeek':
+            return `${formattedValue}% skip a meal four to five times per week.`;
+        case 'always':
+            return `${formattedValue}% skip a meal everyday.`;
+        default:
+            return `${formattedValue}% skip a meal at least once per week.`;
+    }
+};
+
+const SkippedMealsWaffleChart = ({ data }: SkippedMealsWaffleChartProps) => {
+    const [activeGroup, setActiveGroup] = useState<string | null>(null);
+    const skippedMealsData = data?.skippedMeals;
+
+    if (!data || !skippedMealsData) return <>no data :c</>;
+
+    const dataOrder = ['never', 'oncePerWeek', 'twoToThreePerWeek', 'fourToFivePerWeek', 'always'] as const;
+    const waffleData = dataOrder.map((key, index) => ({
+        id: key,
+        value: skippedMealsData[key],
+        label: getLabel(key, skippedMealsData[key]),
+        strength: index === 0 ? 0 : mapRange(index, 1, dataOrder.length - 1, 0, 1),
+        color: index === 0 ? 'var(--white)' : 'var(--red)',
+    }));
+
+    const activeData = waffleData.find((d) => d.id === activeGroup);
+
+    let subtitle = activeData?.label ?? null;
+    subtitle ??= data.skippedMealAtLeastOnce ? getLabel('', data.skippedMealAtLeastOnce) : null;
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.titles}>
+                <span>Meals skipped per week</span>
+                {subtitle && <em className={styles.subtitle}>{subtitle}</em>}
+            </div>
+
+            <div className={styles.chartContainer}>
+                <ChartsWrapper
+                    render={(dimensions) => (
+                        <WaffleChart {...dimensions} data={waffleData} minSize={24} onHover={setActiveGroup} />
+                    )}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default SkippedMealsWaffleChart;
---
+++ src/components/charts/WaffleChart/WaffleChart.tsx
@@ -0,0 +1,137 @@
+import React from 'react';
+import styles from '../Treemap/Treemap.module.scss';
+import * as d3 from 'd3';
+import { range } from 'd3';
+
+export interface WaffleChartData {
+    id: string;
+    value: number;
+    strength: number; // [0, 1]
+    color: string;
+}
+
+export interface WaffleChartProps {
+    width: number;
+    height: number;
+    data: WaffleChartData[];
+    minSize: number;
+    onHover: (id: string | null) => void;
+}
+
+const WaffleChart = ({ width, height, data, minSize, onHover }: WaffleChartProps) => {
+    const margin = { top: 0, right: 0, bottom: 0, left: 0 };
+
+    const effectiveWidth = width - margin.right - margin.left;
+    const xCount = Math.floor(effectiveWidth / minSize);
+    const size = effectiveWidth / xCount;
+
+    const effectiveHeight = height - margin.bottom - margin.top;
+    const yCount = Math.floor(effectiveHeight / size);
+
+    const yRemainder = effectiveHeight % size;
+
+    const xRange = range(xCount).map((n) => '' + n);
+    const xScale = d3
+        .scaleBand()
+        .domain(xRange)
+        .range([margin.left, width - margin.right])
+        .round(false);
+
+    const yRange = range(yCount).map((n) => '' + n);
+    const yScale = d3
+        .scaleBand()
+        .domain(yRange)
+        .range([margin.top + yRemainder, height - margin.bottom])
+        .round(false);
+
+    const totalWaffles = xCount * yCount;
+    const totalValue = data.reduce((acc, d) => acc + d.value, 0);
+
+    let nextOffset = 0;
+    const waffleData = data.map((d) => {
+        const offset = nextOffset;
+        const fraction = d.value / totalValue;
+        const count = Math.round(totalWaffles * fraction);
+
+        nextOffset += count;
+
+        return {
+            count,
+            data: d,
+            offset,
+        };
+    });
+
+    return (
+        <svg width={width} height={height} className={styles.svg}>
+            {waffleData.map(({ count, offset, data }) => {
+                return (
+                    <g key={data.id} onPointerEnter={() => onHover?.(data.id)} onPointerLeave={() => onHover?.(null)}>
+                        {d3.range(count).map((index) => {
+                            const linearIndex = index + offset;
+                            const xIndex = linearIndex % xCount;
+                            const yIndex = Math.floor(linearIndex / xCount);
+
+                            const posX = xScale('' + xIndex)!;
+                            const posY = yScale('' + yIndex)!;
+
+                            return (
+                                <Waffle
+                                    key={index + offset}
+                                    x={posX}
+                                    y={posY}
+                                    xSize={xScale.bandwidth()}
+                                    ySize={yScale.bandwidth()}
+                                    strength={data.strength}
+                                    bg={data.color}
+                                >
+                                    <title>
+                                        index: {index + offset}
+                                        xIndex: {xIndex}; yIndex: {yIndex}
+                                    </title>
+                                </Waffle>
+                            );
+                        })}
+                    </g>
+                );
+            })}
+        </svg>
+    );
+};
+
+export default WaffleChart;
+
+export interface WaffleProps {
+    x: number;
+    y: number;
+    xSize: number;
+    ySize: number;
+    strength: number;
+    bg: string;
+    children: React.ReactNode;
+}
+
+const Waffle = ({ x, y, xSize, ySize, strength, bg, children }: WaffleProps) => {
+    const getRect = (x: number, y: number, xRectSize: number, yRectSize: number) => {
+        return {
+            x: x + (xSize - xRectSize) / 2,
+            y: y + (xSize - xRectSize) / 2,
+            width: xRectSize,
+            height: yRectSize,
+        };
+    };
+
+    return (
+        <g>
+            {strength !== 1 && <rect {...getRect(x, y, xSize, ySize)} fill={bg} shapeRendering={'crispEdges'} />}
+            {strength !== 0 && (
+                <rect
+                    {...getRect(x, y, xSize * strength, ySize * strength)}
+                    fill={'var(--black)'}
+                    shapeRendering={'crispEdges'}
+                />
+            )}
+            {children}
+        </g>
+    );
+};
