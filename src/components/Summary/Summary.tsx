7b32c89441b00088aaa9ad08fb9bc3ae98fc2bc5
DavidGiraldoCode <davidgiraldodesign@gmail.com>
2024-03-09 16:21:28 +0100

    Implement filter buttons for each pisa score & hover

--- src/components/Summary/Summary.tsx
+++ src/components/Summary/Summary.tsx
@@ -1,35 +1,52 @@
+import React from 'react';
 import style from './Summary.module.scss';
 import { CountryDatum } from '../../data/data';
+import { useSelectedCountry } from '../../state/selectedCountry';
+import cn from 'classnames';
+import { usePisaScoreType, setPisaScoreType } from '../../state/pisaScoreType';
 
 export interface SummaryProperties {
     data: { average: number; math: number; reading: number; science: number };
 }
 
 const Summary = ({ data }: SummaryProperties) => {
-    console.log(data);
-
+    const selectedConutry = useSelectedCountry();
+    const selecedType = usePisaScoreType();
+    //console.log(selecedType);
     return (
         <div className={style.summaryCard}>
             <h1>PISA SCORES</h1>
             <p>
-                <span>Latest scores for Italy</span>
+                <span>Latest scores {selectedConutry ? 'for ' + selectedConutry.countryName : 'globally'}</span>
             </p>
             <ul className={style.summaryGrid}>
-                <li className={style.secondary}>
-                    <p>Average</p>
-                    <h4>{Math.round(data.average)}</h4>
+                <li
+                    onClick={() => setPisaScoreType('average')}
+                    className={cn(style.summaryDetail, { [style.active]: selecedType === 'average' })}
+                >
+                    <h4>Average</h4>
+                    <p className={style.score}>{Math.round(data.average)}</p>
                 </li>
-                <li className={style.primary}>
-                    <p>Math</p>
-                    <h4>{Math.round(data.math)}</h4>
+                <li
+                    onClick={() => setPisaScoreType('math')}
+                    className={cn(style.summaryDetail, { [style.active]: selecedType === 'math' })}
+                >
+                    <h4>Math</h4>
+                    <p className={style.score}>{Math.round(data.math)}</p>
                 </li>
-                <li className={style.primary}>
-                    <p>Reading</p>
-                    <h4>{Math.round(data.reading)}</h4>
+                <li
+                    onClick={() => setPisaScoreType('reading')}
+                    className={cn(style.summaryDetail, { [style.active]: selecedType === 'reading' })}
+                >
+                    <h4>Reading</h4>
+                    <p className={style.score}>{Math.round(data.reading)}</p>
                 </li>
-                <li className={style.primary}>
-                    <p>Science</p>
-                    <h4>{Math.round(data.science)}</h4>
+                <li
+                    onClick={() => setPisaScoreType('science')}
+                    className={cn(style.summaryDetail, { [style.active]: selecedType === 'science' })}
+                >
+                    <h4>Science</h4>
+                    <p className={style.score}>{Math.round(data.science)}</p>
                 </li>
             </ul>
         </div>
