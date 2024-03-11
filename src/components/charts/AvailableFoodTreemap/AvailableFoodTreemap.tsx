5575132608fb1384292b1b5cad5f870b03646a48
DavidGiraldoCode <davidgiraldodesign@gmail.com>
2024-03-11 14:44:32 +0100

    Remove console.logs

--- src/components/charts/AvailableFoodTreemap/AvailableFoodTreemap.tsx
+++ src/components/charts/AvailableFoodTreemap/AvailableFoodTreemap.tsx
@@ -20,21 +20,6 @@ const AvailableFoodTreemap = ({ data }: AvailableFoodTreemapProps) => {
         dairyAndEggs: { label: 'Dairy and eggs', keys: ['dairyAndEggs'] },
         sugaryDrinks: { label: 'Sugary drinks', keys: ['sugaryDrinks'] },
         tea: { label: 'Tea', keys: ['tea'] },
-        // grains: { label: 'Grains', keys: ['wheat', 'barley', 'cerealsOther'] },
-        // alcoholicBeverages: { label: 'Alcohol', keys: ['alcoholicBeverages'] },
-        // fat: { label: 'Fats', keys: ['animalFats', 'oilCrops', 'vegetableOils'] },
-        // fish: { label: 'Fish', keys: ['fishAndSeafood'] },
-        // sugar: { label: 'Sugar', keys: ['sugarCrops', 'sugarAndSweeteners'] },
-        // starch: { label: 'Starch', keys: ['starchyRoots'] },
-        // meat: { label: 'Meat', keys: ['otherMeat', 'meatSheepAndGoat', 'meatPig', 'meatPoultry', 'meatBeef'] },
-        // eggs: { label: 'Eggs', keys: ['eggs'] },
-        // milk: { label: 'Milk', keys: ['milk'] },
-        // nuts: { label: 'Nuts', keys: ['nuts'] },
-        // fruit: { label: 'Fruit', keys: ['fruit'] },
-        // vegetables: { label: 'Vegetables', keys: ['vegetables'] },
-        // corn: { label: 'Corn', keys: ['maize'] },
-        // rice: { label: 'Rice', keys: ['rice'] },
-        // pulses: { label: 'Pulses', keys: ['pulses'] },
     };
 
     const hierarchicalData: TreemapData = { id: 'treemap', label: '', children: [] };
@@ -49,13 +34,11 @@ const AvailableFoodTreemap = ({ data }: AvailableFoodTreemapProps) => {
             datum.children = data.map((country) => ({
                 id: foodKey + ':' + country.id,
                 label: country.countryCode,
-                //value: values.keys.reduce((acc, key) => acc + ((country.availableFood as any)[key] || 0), 0),
                 value: values.keys.reduce((acc, key) => acc + ((country.foodInGrams as any)[key] || 0), 0),
                 title: country.countryName,
             }));
         } else if (data.length === 1) {
             const country = data[0];
-            //datum.value = values.keys.reduce((acc, key) => acc + ((country.availableFood as any)[key] || 0), 0);
             datum.value = values.keys.reduce((acc, key) => acc + ((country.foodInGrams as any)[key] || 0), 0);
         } else if (data.length === 0) {
             datum.value = values.keys.reduce((acc, key) => acc + ((metaData.globalAvailableFood as any)[key] || 0), 0);
